4cfdb538e8d90e268952cc6d39dbbdecca4cbb8e
leticiademartinobou <121428742+leticiademartinobou@users.noreply.github.com>
2023-06-23 18:02:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ console.log('hola');
 
 const h1 = document.querySelector('h1');
 const form = document.querySelector('#box');
-const input = document.querySelector('#tareaInput');
 const button1 = document.querySelector('#buttonPlus');
 const ul = document.querySelector('#list');
 const p = document.querySelector('#text2');
-const button2 = document.querySelector('#button-X');
+const button2 = document.querySelector('.button-X');
 
 
 // Función crear tarea
@@ -21,16 +20,31 @@ const button2 = document.querySelector('#button-X');
 //     ul.appendChild("beforeend",elemento)
 // }
 
-function inputTask(tarea) {
-    const elemento = `
-                        <li id="elemento">
-                        <p id="text2">${tarea}</p>
-                        <button class="button-X" data="eliminado" id="0">X</button>
-                        </li>
-                    `
+const taskList = [];
+
+
+function addTask() {
+    const input = document.querySelector('#tareaInput').value;
+    taskList.push(input);
+
+    console.log(taskList);
+    ul.innerHTML = "";
+    for (let i = 0; i < taskList.length; i++) {
+        console.log(taskList[i]);
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `                        
+                                <p id="text2">${taskList[i]}</p>
+                                <button class="button-X" data="eliminado" id="0">X</button>
+                             `
+        ul.appendChild(listItem);
+    }
+    
+    
 }
 
-button1.addEventListener('click', inputTask);
+button1.addEventListener('click', addTask);
+
+
 
 // button1.addEventListener('click',()=> {
 //     const tarea = input.value 
