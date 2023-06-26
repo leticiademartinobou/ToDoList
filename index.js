8a63212677b160a1610568d39b07ee88d10c7a63
leticiademartinobou <121428742+leticiademartinobou@users.noreply.github.com>
2023-06-26 13:11:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 console.log('hola');
 
-const h1 = document.querySelector('h1');
+const mainTitle = document.querySelector('h1');
 const form = document.querySelector('#box');
-const button1 = document.querySelector('#buttonPlus');
+const buttonPlus = document.querySelector('.addButton');
+const buttonRemove = document.querySelector('.removeButton');
+const buttonOrder = document.querySelector('.orderButton');
 const ul = document.querySelector('#list');
 const p = document.querySelector('#text2');
 const button2 = document.querySelector('.button-X');
 
-
-// Función crear tarea
-
-// function agregarTarea(tarea) {
-//     const elemento = `
-//                         <li id="elemento">
-//                         <p id="text2">${tarea}</p>
-//                         <button class="button-X" data="eliminado" id="0">X</button>
-//                         </li>
-//                     `
-//     ul.appendChild("beforeend",elemento)
-// }
 
 const taskList = [];
 
@@ -42,16 +32,49 @@ function addTask() {
     
 }
 
-button1.addEventListener('click', addTask);
+buttonPlus.addEventListener('click', addTask);
+
+function removeTask() {
+    const remove = document.querySelector('#tareaInput').value;
+    taskList.pop(remove);
+    ul.innerHTML = "";
+    for (j = 0; j < taskList.lenght; j++) {
+        console.log(taskList[j]);
+        const removeItem = document.createElement("li");
+        removeItem.innerHTML =  `
+                                    <p id="text2">${taskList[j]}</p>
+                                    <button class="removeButton" data="borrar" id="0">Remove last Task</button>
+                                `
+        ul.removeChild(removeItem);
+                            
+    }
+}
+
+buttonRemove.addEventListener('click', removeTask);
 
 
+function orderTask() {
+    const order = document.querySelector('#tareaInput').value;
+    taskList.sort((order, b) => {
+        if (order < b) {
+            return -1;
+        }
+        if (order > b) {
+            return 1;
+        }
+    })
+    ul.innerHTML = "";
+    for (g = 0; g < taskList.lenght; g++) {
+        console.log(taskList[g]);
+        const orderItem = document.createElement("li");
+        orderItem.innerHTML =  `
+                                    <p id="text2">${taskList[g]}</p>
+                                    <button class="orderButton" data="ordenar" id="0">Order Tasks</button>
+                                `
+        ul.appendChild(orderItem);
+                            
+    }
+}
 
-// button1.addEventListener('click',()=> {
-//     const tarea = input.value 
-//     if (!tarea) {
-//         return;
-//     }
-//     input.value="";
-// })
-
+buttonOrder.addEventListener('click', orderTask);
 
